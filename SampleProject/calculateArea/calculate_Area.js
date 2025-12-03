a216bc755f061a335ee0632c363fe78e82b6f61f
shahidmohsin80 <shahidmohsin1313@gmail.com>
2025-12-03 11:49:00 -0500

    correction

--- SampleProject/calculateArea/calculate_Area.js
+++ SampleProject/calculateArea/calculate_Area.js
@@ -3,7 +3,8 @@ let width;
 
 function calculateArea(){
    length= parseFloat(document.getElementById('length').value);
-   length= parseFloat(document.getElementById('width').value);
-   let area=length*width;
+   width= parseFloat(document.getElementById('width').value);
+   let area=length * width;
+   document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
-document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+log.console(calculateArea());
